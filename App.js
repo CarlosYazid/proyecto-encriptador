3ce95ed2e17963b997242f62af6977e0efd337dd
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2024-01-23 16:39:12 -0500

    first version

---
+++ App.js
@@ -0,0 +1,4 @@
+
+document.getElementById("subtitle_response").innerHTML = "Ningún mensaje fue encontrado";
+
+document.getElementById("text_response").innerHTML = "Ingresa el texto que desees encriptar o desencriptar.";
